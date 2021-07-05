e0112a87ea43725fc1553e4984d4ce2227914fae
Aleksandr <leetvin87@gmail.com>
2021-07-05 13:15:59 +0300

    added date validation in task_10

--- students/litvinov_aleksandr/lesson_1/task_10/task_10.js
+++ students/litvinov_aleksandr/lesson_1/task_10/task_10.js
@@ -5,6 +5,42 @@ let month = document.getElementById('month');
 let year = document.getElementById('year');
 let result = document.getElementById('output');
 
+
+day.addEventListener('keyup', correctDay)
+
+function correctDay() {
+    if (+day.value > 31) {
+        alert('You entered incorrect date');
+        window.location.reload();
+    }
+}
+
+month.addEventListener('keyup', correctDate);
+
+function correctDate() {
+    if (+month.value > 12) {
+        alert('You entered incorrect date');
+        window.location.reload();
+
+    } else if (+day.value > 29 && +month.value === 2) {
+        alert('You entered incorrect date');
+        window.location.reload();
+
+    } else if (+month.value < 8 && +month.value % 2 === 0 && +day.value > 30) {
+        alert('You entered incorrect date');
+        window.location.reload();
+
+    } else if (+month.value < 8 && +month.value % 2 !== 0 && +day.value > 31) {
+        alert('You entered incorrect date');
+        window.location.reload();
+
+    } else if (+month.value >= 8 && +month.value <= 12 && +month.value % 2 !== 0 && +day.value > 30) {
+        alert('You entered incorrect date');
+        window.location.reload();
+    }
+}
+
+
 const button = document.getElementById('button').addEventListener('click', getNextDay);
 
 function getNextDay() {
@@ -27,6 +63,9 @@ function getNextDay() {
         } else if (+day.value === 29 && +year.value % 4 == 0 || (+year.value % 400 == 0 && +year.value % 100 !== 0)) {
             dayRes = 1;
             monthRes = 3;
+        } else if (+day.value === 29 && +month.value === 2 && (+year.value % 4 !== 0 || (+year.value % 400 !== 0 && +year.value % 100 === 0))) {
+            alert(`You entered incorrect date. This year is not leap`);
+            window.location.reload();
         } else {
             dayRes = +day.value + 1;
             monthRes = month.value;
@@ -67,6 +106,7 @@ function getNextDay() {
 
     result.innerHTML = `${dayRes} ${monthRes} ${yearRes}`;
 }
+
 
 
 
