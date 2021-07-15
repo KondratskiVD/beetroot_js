5818d134b708a59ebd5fb96fea03ef628ba6d5cd
ihor_tkachuk <iono19031@gmail.com>
2021-07-15 23:21:38 +0300

    updated lesson25

--- students/ihor_tkachuk/lesson25/task_1.js
+++ students/ihor_tkachuk/lesson25/task_1.js
@@ -8,12 +8,7 @@ function twoNumbers(a,b) {
     if (a < b) {
         return -1;
     }
-    if (a > b) {
-        return 1;
-    }
-    if (a = b) {
-        return 0;
-    }
+    return a - b ? 1 : 0;
 }
 
 let num = twoNumbers(a,b);
