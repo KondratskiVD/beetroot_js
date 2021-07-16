0916e07d7723a28e96155dee7c75be046fb678f5
Valentiago <gerasin1001@gmail.com>
2021-07-16 17:16:33 +0300

    lesson 24 corrections

--- students/valentin_gerasin/lesson-24/Task-6.js
+++ students/valentin_gerasin/lesson-24/Task-6.js
@@ -1,16 +1,23 @@
-while (true) {
-    let a = +prompt('Введите первое число');
-    let b = +prompt('Введите второе число');
-    let op = prompt('Выберете знак: - + / *');
-    let action = {
-        '+': () => a + b,
-        '-': () => a - b,
-        '/': () => a / b,
-        '*': () => a * b
-    }[op];
-    if (action) alert(action());
 
-    if (!confirm('Хотите ли вы решить еще один пример?')) break;
+let a = +prompt('Введите первое число');
+let b = +prompt('Введите второе число');
+let op = prompt('Выберете знак: - + / *');
+
+if (op == '+') {
+    let c = a + b
+    console.log(c);
+} else if (op == '-') {
+    let c = a - b
+    console.log(c);
+} else if (op == '*') {
+    let c = a * b
+    console.log(c);
+} else if (op == '/') {
+    let c = a / b
+    console.log(c);
 }
+
+
+
 
 
