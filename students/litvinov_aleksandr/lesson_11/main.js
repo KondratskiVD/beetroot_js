e237fc960ae0ef08dc2840dc49aca696bc531e2c
Aleksandr <leetvin87@gmail.com>
2021-08-04 23:40:44 +0300

    added task_11 - slider

---
+++ students/litvinov_aleksandr/lesson_11/main.js
@@ -0,0 +1,8 @@
+
+$('.content__slider').slick({
+    infinite: false,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    prevArrow: '<button class="content__slider-btn-prev"><img src="images/arrow-prev.png" alt="prev"></button>',
+    nextArrow: '<button class="content__slider-btn-next"><img src="images/arrow-next.png" alt="next"></button>',
+});
