ac97b535c055fbc24aaa15d756dc008fb9c135d6
Aleksandr <leetvin87@gmail.com>
2021-08-23 00:45:28 +0300

    added lesson_16-Vue and lesson_17-Vue-Component

---
+++ students/litvinov_aleksandr/lesson_17/app/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Number: {{ counter }}</h3>
+  </div>
+</template>
+
+<script>
+import { eventEmitter } from "../main";
+export default {
+  props: ["counter"],
+  data() {
+    return {
+      currentCounter: this.counter,
+    };
+  },
+  mounted() {
+    eventEmitter.$on("addCount", () => {
+      this.counter++;
+    }),
+      eventEmitter.$on("decCount", () => {
+        this.counter--;
+      });
+  },
+};
+</script>
